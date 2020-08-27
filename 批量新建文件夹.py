9915d863a5d8a0f39028e9661bceadda6215a513
mikeinzhagi <mikeinzhagi@163.com>
2020-08-27 13:50:25 +0800

    增加新建文件夹

--- 批量新建文件夹.py
+++ 批量新建文件夹.py
@@ -2,4 +2,5 @@ import os
 meilong = ['爸爸', '妈妈', '老婆', '皮蛋', '大王']
 for i in range(len(meilong)):
     print(meilong[i])
-    os.makedirs(meilong[i])
+    os.chdir(r'e:\python练习')   # 指定文件夹目录(目录不存在程序就无法运行)
+    os.makedirs(meilong[i])    # 根据列表内容建立文件夹
